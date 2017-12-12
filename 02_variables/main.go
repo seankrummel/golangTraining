223309563494923c7db48586d731c269752eea38
Sean Krummel <sean.krummel@gmail.com>
2017-12-12 16:45:33 -0600

    learning about scope

--- 02_variables/main.go
+++ 02_variables/main.go
@@ -2,15 +2,24 @@ package main
 
 import "fmt"
 
+var x = 100 // package level scope
+
 func main() {
 
-	// shorthand declare and assign
+	f1()
+	// function expression
+	f2 := func() /* anonymous function */ {
+		fmt.Printf("%v \n", x)
+	}
+	f2()
+
+	// shorthand declare and assign, block level scope
 	a := 10
 	b := "golang"
 	c := 4.17
 	d := true
 
-	fmt.Printf("%v \t %T \n", a, a)
+	fmt.Printf("%v \t %T \n", a, a) // %v is value, %T is type
 	fmt.Printf("%v \t %T \n", b, b)
 	fmt.Printf("%v \t %T \n", c, c)
 	fmt.Printf("%v \t %T \n", d, d)
@@ -25,4 +34,8 @@ func main() {
 	fmt.Printf("%v \n", f)
 	fmt.Printf("%v \n", g)
 	fmt.Printf("%v \n", h)
+}
+
+func f1() {
+	fmt.Printf("%v \n", x)
 }
